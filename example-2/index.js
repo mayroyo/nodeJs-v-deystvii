d29c72760e1f278b29c0c32d6a02596dbc923bab
JamesBerlioz <mavlut18@gmail.com>
2023-09-12 19:00:15 +0300

    callback and EventEmitter

---
+++ example-2/index.js
@@ -0,0 +1,28 @@
+const events = require("events");
+const net = require("net");
+
+const channel = new events.EventEmitter();
+
+channel.clients = {};
+channel.subscriptions = {};
+
+channel.on("join", function (id, client) {
+  this.clients[id] = client;
+  this.subscriptions[id] = (senderld, message) => {
+    if (id != senderld) {
+      this.clients[id].write(message);
+    }
+  };
+  this.on("broadcast", this.subscriptions[id]);
+});
+
+const server = net.createServer((client) => {
+  const id = `${client.remoteAddress}:${client.remotePort}`;
+  channel.emit("join", id, client);
+  client.on("data", (data) => {
+    data = data.toString();
+    channel.emit("broadcast", id, data);
+  });
+});
+
+server.listen(8888);
